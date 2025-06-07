17e434095198fceadafe778cb65ef61854b5c05f
Henri Sota <sotahenry@gmail.com>
2025-06-07 20:24:17 +0200

    setup development environment

---
+++ shells/default/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  inputs,
+  pkgs,
+  ...
+}:
+inputs.devenv.lib.mkShell {
+  inherit inputs pkgs;
+  modules = [
+    {
+      packages = with pkgs; [
+        zola
+      ];
+
+      languages.nix.enable = true;
+
+      pre-commit.hooks = {
+        editorconfig-checker.enable = false;
+        end-of-file-fixer.enable = true;
+        trim-trailing-whitespace.enable = true;
+
+        lychee.enable = true;
+        markdownlint.enable = true;
+        mkdocs-linkcheck.enable = true;
+
+        alejandra.enable = true;
+        deadnix = {
+          enable = true;
+          args = ["--edit"];
+        };
+        statix = {
+          enable = true;
+          args = ["fix" "-i" ".direnv"];
+        };
+      };
+    }
+  ];
+}
